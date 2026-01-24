b83239e3a1f0cbd921f2525c830dd41c1d584625
Ivan Vladimir Meza Ruiz <ivanvladimir@gmail.com>
2026-01-24 01:50:42 -0600

    feat: adds legent of forgot password

--- src/app/front/routes/user.py
+++ src/app/front/routes/user.py
@@ -98,6 +98,22 @@ async def register(request: Request) -> HTMLResponse:
     )
     return response
 
+@router.get("/forgot_password")
+async def forgot_password(request: Request) -> HTMLResponse:
+    """
+    Forma para capturar el correo del usuario que olvido su contraseña
+    """
+    start_time = time.time()
+    response = templates.TemplateResponse(
+        request=request,
+        name="user/forgot_password.html",
+        context={
+            "elapsed_time_seconds": f"{time.time() - start_time:2.3f}",
+            "active_page": 'register',
+        },
+    )
+    return response
+
 @router.get("/email_verification")
 async def email_verification(
         request: Request,
